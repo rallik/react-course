2dc4299ea8342dc3607fa5ad0fac47c3b2f886d3
Reuben T Allik <reubenallik@gmail.com>
2021-03-09 20:13:45 -0500

    @1:42:59

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # react-course
  -> 3/9 - 1:04:13 - Text Editor Setup
+        - 1:42:59 - CSS Basics
--- fcc-tutorial/src/index.js
+++ fcc-tutorial/src/index.js
@@ -2,8 +2,39 @@ import React from 'react';
 import ReactDom from 'react-dom';
 
 
-function Hello(){
-  return <h1>Hello World</h1>;
+function Booklist(){
+  return (
+    <section>
+      <Book/>
+      <Book/>
+      <Book/>
+      <Book/>
+      <Book/>
+      <Book/>
+    </section>
+  );
 }
 
-ReactDom.render(<Hello />, document.getElementById('root'))
+
+const Book = () => {
+  return (
+    <article>
+    <Image></Image>
+    <Title/>
+    <Author/>
+  </article>
+  )
+}
+
+const Image = () => (
+  <img 
+    src="https://images-na.ssl-images-amazon.com/images/I/41KMmXBPckL._SX327_BO1,204,203,200_.jpg"
+    alt="The Code Breaker by Walter Isaacson"
+  />
+)
+
+const Title = () => <h1>The Code Breaker</h1>;
+
+const Author = () => <h4>Walter Isaacson</h4>;
+
+ReactDom.render(<Booklist />, document.getElementById('root'))
