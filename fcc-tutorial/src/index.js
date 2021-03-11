3174e7d1631bad1f33c9371d2d3b25b148758d3c
Reuben T Allik <reubenallik@gmail.com>
2021-03-10 20:01:18 -0500

    booklist

--- fcc-tutorial/src/index.js
+++ fcc-tutorial/src/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import './index.css';
 
+//setup vars
+const title = 'The Code Breaker';
+const author = 'Walter Isaacson';
+const source = "https://images-na.ssl-images-amazon.com/images/I/41KMmXBPckL._SX327_BO1,204,203,200_.jpg";
+
+function App(){
+  return (
+    <main>
+      <h1>The Booklist</h1>
+      <Booklist />
+    </main>
+  );
+}
 
 function Booklist(){
   return (
-    <section>
-      <Book/>
+    <section className='booklist'>
+      <Book author/>
       <Book/>
       <Book/>
       <Book/>
@@ -16,25 +30,20 @@ function Booklist(){
 }
 
 
-const Book = () => {
+const Book = (props) => {
+  console.log(props);
   return (
-    <article>
-    <Image></Image>
-    <Title/>
-    <Author/>
-  </article>
+    <article className='book'>
+      <img 
+        src={source}
+        alt=""
+      />
+      <h3>{title}</h3>
+  <h4 style={{color:'#617d98'}}>{author}</h4>
+    </article> 
   )
 }
 
-const Image = () => (
-  <img 
-    src="https://images-na.ssl-images-amazon.com/images/I/41KMmXBPckL._SX327_BO1,204,203,200_.jpg"
-    alt="The Code Breaker by Walter Isaacson"
-  />
-)
 
-const Title = () => <h1>The Code Breaker</h1>;
 
-const Author = () => <h4>Walter Isaacson</h4>;
-
-ReactDom.render(<Booklist />, document.getElementById('root'))
+ReactDom.render(<App />, document.getElementById('root'))
