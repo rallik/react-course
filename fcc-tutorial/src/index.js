3d13d278e2010999d0692d2b00f261c1f971a580
Reuben T Allik <reubenallik@gmail.com>
2021-03-10 20:11:08 -0500

    adding props

--- fcc-tutorial/src/index.js
+++ fcc-tutorial/src/index.js
@@ -3,29 +3,31 @@ import ReactDom from 'react-dom';
 import './index.css';
 
 //setup vars
-const title = 'The Code Breaker';
-const author = 'Walter Isaacson';
-const source = "https://images-na.ssl-images-amazon.com/images/I/41KMmXBPckL._SX327_BO1,204,203,200_.jpg";
-
-function App(){
-  return (
-    <main>
-      <h1>The Booklist</h1>
-      <Booklist />
-    </main>
-  );
+const firstbook = {
+  title: 'The Code Breaker',
+  author: 'Walter Isaacson',
+  source: "https://images-na.ssl-images-amazon.com/images/I/41KMmXBPckL._SX327_BO1,204,203,200_.jpg"
 }
+
+const secondbook = {
+  title: 'The Code Breaker2',
+  author: 'Walter Isaacsonnnn',
+  source: "https://images-na.ssl-images-amazon.com/images/I/41KMmXBPckL._SX327_BO1,204,203,200_.jpg"
+}
+
+
+
 
 function Booklist(){
   return (
-    <section className='booklist'>
-      <Book author/>
-      <Book/>
-      <Book/>
-      <Book/>
-      <Book/>
-      <Book/>
-    </section>
+    <main>
+      <h1>The Booklist</h1>
+      <section className='booklist'>
+        <Book source={firstbook.source} title={firstbook.title} author={firstbook.author}/>
+        <Book source={secondbook.source} title={secondbook.title} author={secondbook.author}/>
+
+      </section>
+    </main>
   );
 }
 
@@ -35,15 +37,15 @@ const Book = (props) => {
   return (
     <article className='book'>
       <img 
-        src={source}
+        src={props.source}
         alt=""
       />
-      <h3>{title}</h3>
-  <h4 style={{color:'#617d98'}}>{author}</h4>
+      <h3>{props.title}</h3>
+  <h4 style={{color:'#617d98'}}>{props.author}</h4>
     </article> 
   )
 }
 
 
 
-ReactDom.render(<App />, document.getElementById('root'))
+ReactDom.render(<Booklist />, document.getElementById('root'))
