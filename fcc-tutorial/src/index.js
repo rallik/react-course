3230f193d9233494202ae1f9e1144c5a66668141
Reuben T Allik <reubenallik@gmail.com>
2021-03-11 22:18:08 -0500

    events

--- fcc-tutorial/src/index.js
+++ fcc-tutorial/src/index.js
@@ -3,69 +3,67 @@ import ReactDom from 'react-dom';
 import './index.css';
 
 //setup vars
-const firstbook = {
-  title: 'The Code Breaker',
-  author: 'Walter Isaacson',
-  source: "https://images-na.ssl-images-amazon.com/images/I/41KMmXBPckL._SX327_BO1,204,203,200_.jpg"
+  const books = [
+  {
+    id: 1,
+    title: 'The Code Breaker',
+    author: 'Walter Isaacson',
+    source: "https://images-na.ssl-images-amazon.com/images/I/41KMmXBPckL._SX327_BO1,204,203,200_.jpg"
+  },
+  {
+    id: 2,
+    title: 'The Very Hungry Caterpillar',
+    author: 'Eric Carle',
+    source: "https://images-na.ssl-images-amazon.com/images/I/71KilybDOoL._AC_UL200_SR200,200_.jpg"
+  },
+  {
+    id: 3,
+    title: 'The Code Breaker2',
+    author: 'Stephen King',
+    source: "https://images-na.ssl-images-amazon.com/images/I/81LoD65Jy6L._AC_UL200_SR200,200_.jpg"
+  }
+]
+
+function App() {
+  return ( 
+    <main>
+      <h1>The Booklist</h1>
+      <Booklist />
+    </main>
+
+  );
 }
-
-const secondbook = {
-  title: 'The Very Hungry Caterpillar',
-  author: 'Eric Carle',
-  source: "https://images-na.ssl-images-amazon.com/images/I/71KilybDOoL._AC_UL200_SR200,200_.jpg"
-}
-
-const thirdbook = {
-  title: 'The Code Breaker2',
-  author: 'Stephen King',
-  source: "https://images-na.ssl-images-amazon.com/images/I/81LoD65Jy6L._AC_UL200_SR200,200_.jpg"
-}
-
-
 
 
 function Booklist(){
   return (
-    <main>
-      <h1>The Booklist</h1>
-      <section className='booklist'>
-        <Book
-          source={firstbook.source}
-          title={firstbook.title}
-          author={firstbook.author}
-        >
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis exercitationem repudiandae quae facilis consequuntur magnam autem soluta consequatur minima mollitia.</p>
-        </Book>
-        <Book
-          source={secondbook.source}
-          title={secondbook.title}
-          author={secondbook.author}
-        />
-        <Book
-          source={thirdbook.source}
-          title={thirdbook.title}
-          author={thirdbook.author}
-        />
-
-      </section>
-    </main>
+    <section className='booklist'>
+      {books.map((book) => {
+        return (
+          <Book key={book.id} {...book} />
+        );
+      })}
+    </section>
   );
 }
 
 
 const Book = (props) => {
   // console.log(props);
-  const { source, title, author, children } = props;
+  const { source, title, author } = props;
+  const clickHandler = () => {
+    alert('hello world')
+  }
   return (
     <article className='book'>
-      <img src={source} alt=""/>
+      <img onClick={() => console.log('this works too')} src={source} alt=''/>
       <h3>{title}</h3>
       <h4>{author}</h4>
-      {children}
+      {/* <button type='button' onClick={clickHandler}>Button</button> */}
     </article> 
   )
 }
 
 
 
-ReactDom.render(<Booklist />, document.getElementById('root'))
+ReactDom.render(<App />, document.getElementById('root'))
